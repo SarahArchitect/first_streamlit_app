f9887464c1f8d988980847a7cc20eb182bc13278
SarahArchitect <108442008+SarahArchitect@users.noreply.github.com>
2022-06-29 21:20:51 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,5 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 import streamlit
 streamlit.title('Breakfast Menu')
+import streamlit
+streamlit.title('Omega 3 & Blueberry Oatmeal')
